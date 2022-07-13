1f3c8de8162f0a7b3bc1c6c7ec23a41a020fc57b
Sergey74ua <Sergey74ua@gmail.com>
2022-07-13 14:08:52 +0300

    cs

---
+++ Kata_8 Convert a String to a Number.cs
@@ -0,0 +1,19 @@
+﻿// Convert a Number to a String!
+
+using System;
+
+class Kata
+{
+	public static int StringToNumber(String str)
+	{
+		return Convert.ToInt32(str);
+	}
+
+	// Для локального запуска
+	static void Main(string[] args)
+	{
+		string test = "123";
+		Console.WriteLine(StringToNumber(test));
+		Console.ReadKey();
+	}
+}
